4f4a2523c16f4202b3ec5f9777d9fa45b9e37dc8
ritesh301 <riteshpradhan301@gmail.com>
2025-04-25 20:38:20 +0530

    updated db.js for deployment

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
-require('dotenv').config();
-
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
+module.exports = connection;
 
-module.exports = pool.promise();
